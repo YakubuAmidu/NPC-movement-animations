fa1e8881740bc0d8e01fe8cbaa7ebad2df0d7c23
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2023-06-15 11:32:32 -0700

    Initial commit

--- script.js
+++ script.js
@@ -5,17 +5,21 @@ const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
 
+const numberOfEnemies = 100;
+const enemiesArray = [];
+
 class Enemy {
     constructor(){
-        this.x = 10;
-        this.y = 50;
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
         this.width = 100;
         this.height = 100;
+        this.speed = Math.random() * 4 - 2;
     }
 
     update(){
-        this.x++;
-        this.y++;
+        this.x += this.speed;
+        this.y += this.speed;
     }
 
     draw(){
@@ -23,12 +27,19 @@ class Enemy {
     }
 }
 
-const enemy1 = new Enemy();
+for (let i = 0; i < numberOfEnemies; i++){
+    enemiesArray.push(new Enemy());
+};
+
+console.log('EnemiesArray: ', enemiesArray);
 
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    enemy1.update();
-    enemy1.draw();
+
+    enemiesArray.forEach(enemy => {
+        enemy.update();
+        enemy.draw();
+    })
     requestAnimationFrame(animate);
 };
 
