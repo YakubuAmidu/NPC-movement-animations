fc4b8fc2ac25cbc0a707ca758c17871394e4891a
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2023-06-15 10:58:12 -0700

    initial commit

--- script.js
+++ script.js
@@ -15,12 +15,16 @@ class Enemy {
 }
 
 const enemy1 = new Enemy();
+const enemy2 = new Enemy();
 
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     enemy1.x++;
     enemy1.y++;
+    enemy2.x += 2;
+    enemy2.y += 2;
     ctx.fillRect(enemy1.x, enemy1.y, enemy1.width, enemy1.height);
+    ctx.fillRect(enemy2.x, enemy2.y, enemy2.width, enemy2.height);
     requestAnimationFrame(animate);
 };
 
